15e8e8fbf5623a9b2aed7a884ce6849528963202
Charlie <kiwichang2017@gmail.com>
2022-01-25 10:23:44 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 const DiscordWebhook = require('discord.js').WebhookClient
 const Koa = require('koa');
 const app = new Koa();
-const webhook = new DiscordWebhook('925019664042590219','KVdT1pdwSc8YQBazzeZhsKigohu3tCcAHJ7x1tvO8K7kq-BoNGpTQ9dgdZi9eUk1QoqS')
+const webhook = new DiscordWebhook('Re','moved')
 
 // response
 app.use(ctx => {
